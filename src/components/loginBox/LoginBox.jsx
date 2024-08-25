145f48c4f4f3af9b0ab68886e3446236da7c0c03
Aarsho <chatterjeeaarsho@gmail.com>
2024-08-25 14:42:50 +0200

    auth added

--- src/components/loginBox/LoginBox.jsx
+++ src/components/loginBox/LoginBox.jsx
@@ -1,9 +1,21 @@
+'use client'
 import React from 'react'
 import styles from './loginBox.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 function LoginBox() {
+    const {status} = useSession()
+    console.log(status)
+    const router = useRouter()
+    if (status === "authenticated") {
+        router.push("/")
+    }
+    if (status === "unauthenticated") {
+        router.push("/login")
+    }
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
@@ -25,7 +37,7 @@ function LoginBox() {
                         <button type='submit' className={styles.submitBtn}>Log in</button>
                     </form>
                     <div className={styles.or}>OR</div>
-                    <button className={styles.loginBtn}>Continue with Google</button>
+                    <button className={styles.loginBtn} onClick={() => signIn("google")}>Continue with Google</button>
                     <div className={styles.register}>
                         <div>New user?</div>
                         <Link href='.'>Register</Link>
